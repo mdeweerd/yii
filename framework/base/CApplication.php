e0fd764bdb85ab94f8ba080a1c2bc29235a60d72
MDW <mdeweerd@users.noreply.github.com>
2026-06-15 10:03:42 +0200

    :bulb: Add PHPDoc return type annotations to CApplication::end()
    Added PHPDoc return type annotations to the end() method in CApplication.php to improve static analysis.

--- framework/base/CApplication.php
+++ framework/base/CApplication.php
@@ -194,6 +194,9 @@ abstract class CApplication extends CModule
 	 * @param integer $status exit status (value 0 means normal exit while other values mean abnormal exit).
 	 * @param boolean $exit whether to exit the current request. This parameter has been available since version 1.1.5.
 	 * It defaults to true, meaning the PHP's exit() function will be called at the end of this method.
+	 * @return never|void
+         * @phan-return ($exit ? never : void)
+         * @phpstan-return ($exit ? never : void)
 	 */
 	public function end($status=0,$exit=true)
 	{
